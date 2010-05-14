66e171ba42c5c61887b2b03445e9792fb6f5d94b
Patrick Huesler <patrick.huesler@gmail.com>
2010-05-14 15:33:33 +0200

    Fixed nasty bug with receivedData not set to nil

--- ApplicationController.rb
+++ ApplicationController.rb
@@ -1,28 +1,55 @@
 require 'json'
 
 class ApplicationController
-  attr_accessor :tweetsTableView
+  YES = true
+  NO = false
+  
+  attr_accessor :tweetsTableView, :reload
+  
+  def init
+    if !super
+      return nil
+    else
+      @refresh_url = nil
+      @timeline = []
+      return self
+    end
+  end
   
   def applicationDidFinishLaunching(notification)
     NSLog("yeah")
-    url = NSURL.URLWithString("http://twitter.com/statuses/public_timeline.json")
-    request = NSURLRequest.requestWithURL(url)
-    NSURLConnection.connectionWithRequest(request, delegate: self)
-  end
-  
-  def timeline
-    @timeline ||= []
+    refreshTweets
+    @timer = NSTimer.scheduledTimerWithTimeInterval(30,
+                                                    target: self,
+                                                    selector: :refreshTweets,
+                                                    userInfo: nil,
+                                                    repeats: YES)
   end
   
   def connectionDidFinishLoading(connection)
     NSLog("finished loading")
     responseString = NSString.alloc.initWithData(@receivedData, encoding: NSUTF8StringEncoding)
     statuses = JSON.parse(responseString)
-    statuses.each do |status|
-     image = NSImage.alloc.initWithContentsOfURL(NSURL.URLWithString(status['user']['profile_image_url']))
-     @timeline.push({image: image,  tweet: status["text"]})
+    @receivedData = nil
+    @timeline = []
+    statuses['results'].each do |status|
+     image = NSImage.alloc.initWithContentsOfURL(NSURL.URLWithString(status['profile_image_url']))
+     @timeline << {image: image,  tweet: status["text"]}
     end
+    @refresh_url = statuses['refresh_url']
+    NSLog(statuses['results'].first["text"])
     self.tweetsTableView.reloadData
+  end
+  
+  def refreshTweets
+    if @refresh_url
+      url = NSURL.URLWithString("http://twitter.com/search.json?#{@refresh_url}")
+    else
+      url = NSURL.URLWithString("http://twitter.com/search.json?q=%23Telegraaf")
+    end
+
+    request = NSURLRequest.requestWithURL(url, cachePolicy: NSURLRequestReloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 60.0)
+    NSURLConnection.connectionWithRequest(request, delegate: self)
   end
   
   def connection(connection, didReceiveResponse: response)
@@ -38,13 +65,23 @@ class ApplicationController
   
   def connection(conn, didFailWithError:error)
     NSLog("FAILED")
+    NSLog(error.localizedDescription)
+  end
+  
+  def reload(sender)
+    refreshTweets
   end
   
   def numberOfRowsInTableView(tableView)
-    timeline.size
+    @timeline.size
   end
   
   def tableView(tableView, objectValueForTableColumn: column, row: row)
-    return timeline[row].valueForKey(column.identifier.to_sym)
+    return @timeline[row].valueForKey(column.identifier.to_sym)
+  end
+  
+  def applicationShouldTerminate(sender)
+    @timer.invalidate
+    return NSTerminateNow
   end
 end
